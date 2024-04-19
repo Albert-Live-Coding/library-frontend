28a234ede751a1e8884e39b7b1e6392221b68edd
AlbertLeipzig <albertclemvill@gmail.com>
2024-04-19 10:35:24 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from './components/Header';
 
 /* import FetchData from './utils/FetchData';
 FetchData().then((data) => console.log(data)); */
@@ -9,4 +9,4 @@ export const App = () => {
       <h1>Hello There</h1>
     </>
   );
-;
+};
