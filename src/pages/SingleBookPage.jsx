5b3699ced3a52fd28b299cdf15cae9598d1c50f6
elvira492 <elvira.dederer.ed@gmail.com>
2024-04-19 11:29:20 +0200

    single book page finished

---
+++ src/pages/SingleBookPage.jsx
@@ -0,0 +1,33 @@
+import FetchData from "../utils/FetchData";
+import BookComponent from "../components/Book-Elvira";
+import { useState, useEffect } from "react";
+
+const SingleBookPage = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const data = await FetchData(); //FETCHDATA hier
+        setBooks(data);
+      } catch (error) {
+        console.error("Error!", error);
+      } //Fehlermeldung
+    };
+
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Book Details</h1>
+      <div>
+        {books.map((book) => (
+          <BookComponent book={book} /> //Component + Prop der an Book-Elvira gebunden ist
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SingleBookPage;
