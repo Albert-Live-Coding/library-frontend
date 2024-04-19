9df9b1061ea49343301cca470cc0894cd6f72b15
Sedigheh Balyavi <bayavisedigheh@gmail.com>
2024-04-19 11:58:42 +0200

    added login-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
-
+import { Outlet } from 'react-router-dom';
+import LoginForm from './components/LoginForm.jsx';
 /* import FetchData from './utils/FetchData';
 FetchData().then((data) => console.log(data)); */
 
@@ -7,7 +8,8 @@ export const App = () => {
   return (
     <>
       <Header />
-      <h1>Hello There</h1>
+      <Outlet />
+      <LoginForm />
     </>
   );
 };
