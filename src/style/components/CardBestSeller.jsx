507cd3345542c88adc644d049c39231a43adce68
Nelly <nelly.fuerst@outlook.de>
2024-04-19 11:34:26 +0200

    user card added

--- src/style/components/CardBestSeller.jsx
+++ src/style/components/CardBestSeller.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CardBestSeller = () => {
   return (
-    <div className="book-card" style={}>
+    <div className="book-card" style= "">
       <img src={book.cover} alt="" />
       <h1>{book.releaseYear}</h1>
       <p>{book.author}</p>
---
+++ src/style/components/userPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const userPage = () => {
+    const user = {
+        firstName: "John",
+        lastName: "Doe",
+        address: "Main Street,20, 01234 Main City"
+    }
+
+  return (
+    <div>userPage</div>
+  )
+}
+
+export default userPage
