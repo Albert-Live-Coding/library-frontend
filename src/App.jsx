da1eb9145a54a4aedd8ad7a7095c34f8468cf306
Nelly <nelly.fuerst@outlook.de>
2024-04-19 10:12:29 +0200

    first entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import CardBestSeller from "./style/components/CardBestSeller";
+
 export const App = () => {
   return (
     <>
       <h1>Hello There</h1>
+      <CardBestSeller/>
     </>
   );
 };
