41fe0174b098ff44b9ad1294aec9d1e98b8d3133
Baris <barisbalcimusic@gmail.com>
2024-04-19 11:34:33 +0200

    category-page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
-import Header from './components/Header';
+// import Header from './components/Header';
+
+// /* import FetchData from './utils/FetchData';
+// FetchData().then((data) => console.log(data)); */
+
+// export const App = () => {
+//   return (
+//     <>
+//       <Header />
+//       <h1>Hello There</h1>
+//     </>
+//   );
+// };
+
+import Header from "./components/Header";
+import CategoryPage from "./pages/CategoryPage";
 
 /* import FetchData from './utils/FetchData';
 FetchData().then((data) => console.log(data)); */
@@ -6,8 +21,7 @@ FetchData().then((data) => console.log(data)); */
 export const App = () => {
   return (
     <>
-      <Header />
-      <h1>Hello There</h1>
+      <CategoryPage />
     </>
   );
 };
---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import FetchData from "../utils/FetchData";
+import Header from "../components/Header";
+import BookElvira from "../components/Book-Elvira";
+
+const CategoryPage = () => {
+  const [bookdata, setBookData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await FetchData();
+        setBookData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [bookdata]);
+
+  return (
+    <>
+      <Header />
+      {bookdata.map((book) => {
+        <BookElvira book={book} />;
+      })}
+    </>
+  );
+};
+
+export default CategoryPage;
