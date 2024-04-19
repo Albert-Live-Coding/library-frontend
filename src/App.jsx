56f3670bea0aef9dfb8619a90562b5eca8403de9
Baris <barisbalcimusic@gmail.com>
2024-04-19 10:30:24 +0200

    fertig

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export const App = () => {
-  return (
-    <>
-      <h1>Hello There</h1>
-    </>
-  );
+// export const App = () => {
+//   return (
+//     <>
+//       <h1>Hello There</h1>
+//     </>
+//   );
+// };
+import Header from "./components/Header";
+
+const App = () => {
+  return <Header />;
 };
+
+export default App;
